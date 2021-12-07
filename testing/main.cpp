c2f78812122333d26c25aa30a97334c8a62ee2ab
radj307 <radj307@gmail.com>
2021-12-07 15:48:48 -0500

    add testing

---
+++ testing/main.cpp
@@ -0,0 +1,15 @@
+#include <strconcepts.hpp>
+#include <strlocale.hpp>
+#include <strutility.hpp>
+#include <strconv.hpp>
+#include <str.hpp>
+
+int main(const int argc, char** argv)
+{
+	try {
+
+		return 0;
+	} catch (...) {
+		return -2;
+	}
+}
