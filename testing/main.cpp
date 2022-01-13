be8b78615d0301b28f9157d453f168f36d687d72
radj307 <radj307@gmail.com>
2022-01-13 15:53:24 -0500

    Update testing

--- testing/main.cpp
+++ testing/main.cpp
@@ -1,37 +1,41 @@
 #include <TermAPI.hpp>
+#include <xlog.hpp>
 
 #include <fileio.hpp>
 #include <TokenRedux.hpp>
 #include <TokenReduxDefaultDefs.hpp>
 #include <INIRedux.hpp>
+#include <env.hpp>
 
+#include <iostream>
+
+#include <redirect.hpp>
+#include <xlog.hpp>
 
 int main(const int argc, char** argv)
 {
 	try {
+		using namespace color;
 		std::cout << term::EnableANSI;
 
 		file::ini::INI ini{ "test.ini" };
 
-		if constexpr (var::same_or_convertible<int, int>) {
-			std::cout << ini << std::endl;
-		}
-
-
-		//file::write("test.txt", "Hello", ' ', "World!");
-		//auto ss{ file::read("test.txt") };
-		//std::cout << ss.rdbuf() << std::endl;
-		//return 0;
-		//using namespace file::base;
-
-		//auto vec{ TkParse(Tkiz(file::read("test.txt")).tokenize()).parse() };
+		// CHECK ENV FOR TERM SUPPORT
+		#ifdef OS_WIN
+		//true
+		#elif OS_LINUX
+		if (const auto var{ env::getvar("TERM") }; var.has_value())
+			std::cout << setcolor::green << var.value() << setcolor::reset_f << std::endl;
+		else std::cout << setcolor::red << "[NOT FOUND]" << setcolor::reset_f << std::endl;
+		#endif
+		// CHECK ENV FOR TERM SUPPORT
 
 		return 0;
 	} catch (const std::exception& ex) {
-		std::cerr << term::error << ex.what() << std::endl;
+		std::cerr << xlog::error << ex.what() << std::endl;
 		return -1;
 	} catch (...) {
-		std::cerr << term::error << "An unknown exception occurred!" << std::endl;
+		std::cerr << xlog::error << "An unknown exception occurred!" << std::endl;
 		return -2;
 	}
 }
