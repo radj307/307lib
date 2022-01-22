8fb6f29ba0f26c97d81994716c1dfdad50d34099
radj307 <radj307@gmail.com>
2022-01-22 17:07:43 -0500

    fix bug in INIRedux.hpp where keys might have whitespace

--- filelib/include/INIRedux.hpp
+++ filelib/include/INIRedux.hpp
@@ -487,7 +487,7 @@ namespace file::ini {
 					else
 						ss.seekg(pos - 1ll);
 					// getsimilar
-					return Token{ getsimilar(LEXEME::LETTER_LOWERCASE, LEXEME::LETTER_UPPERCASE, LEXEME::UNDERSCORE, LEXEME::SUBTRACT, LEXEME::PERIOD, LEXEME::COMMA, LEXEME::DIGIT, LEXEME::WHITESPACE), TokenType::KEY };
+					return Token{ str::strip_line(getsimilar(LEXEME::LETTER_LOWERCASE, LEXEME::LETTER_UPPERCASE, LEXEME::UNDERSCORE, LEXEME::SUBTRACT, LEXEME::PERIOD, LEXEME::COMMA, LEXEME::DIGIT, LEXEME::WHITESPACE), "#;"), TokenType::KEY};
 				}
 				case LEXEME::SUBTRACT: [[fallthrough]]; // number start
 				case LEXEME::DIGIT:
