de90e44b0972047becd007ebabf8cdb1b65e5c0a
radj307 <radj307@gmail.com>
2022-01-22 17:02:07 -0500

    except::message can be modified in derived object

--- shared/include/make_exception.hpp
+++ shared/include/make_exception.hpp
@@ -10,14 +10,49 @@
  * @brief	Exception object with a custom message stored as a member variable.
  */
 class except : public std::exception {
-	const std::string _message;
+protected:
+	/// @brief	The message associated with this exception
+	std::string message;
 
 public:
+	/**
+	 * @brief	Default constructor with a blank message.
+	 */
 	except() = default;
-	except(auto&& message) : _message{ std::forward<decltype(message)>(message) } {}
-	const char* what() const noexcept override { return _message.c_str(); }
+	/**
+	 * @brief			Constructor with a message.
+	 * @param message	A string to print when calling the what() method.
+	 */
+	except(auto&& message) : message{ std::forward<decltype(message)>(message) } {}
+	/**
+	 * @brief	Retrieve the message associated with this exception.
+	 * @returns	const char*
+	 */
+	virtual const char* what() const noexcept override { return message.c_str(); }
+	/**
+	 * @brief		Insert the message associated with this exception into an output stream.
+	 *\n			This is the same as calling the what() method.
+	 * @param os	Target Output Stream Instance
+	 * @param ex	Constant Reference of an except object.
+	 * @returns		std::ostream&
+	 */
+	friend std::ostream& operator<<(std::ostream& os, const except& ex) { return os << ex.what(); }
 };
 
+/**
+ * @brief				Create a custom exception type with the given arguments as a message.
+ * @tparam ReturnT		The type of exception to return.
+ * @tparam ...Ts		Variadic Templated Types.
+ * @param ...message	The message shown when calling the what() function.
+ * @returns				ReturnT
+ */
+template<std::derived_from<except> ReturnT, var::Streamable<std::stringstream>... Ts>
+WINCONSTEXPR ReturnT make_custom_exception(Ts&&... message)
+{
+	std::stringstream ss;
+	(ss << ... << std::forward<Ts>(message));
+	return ReturnT{ ss.str() };
+}
 /**
  * @brief				Create a custom exception type with the given arguments as a message.
  * @tparam ReturnT		The type of exception to return.
@@ -25,17 +60,16 @@ public:
  * @param ...message	The message shown when calling the what() function.
  * @returns				ReturnT
  */
-template<std::derived_from<std::exception> ReturnT, var::Streamable<std::stringstream>... Ts>
-WINCONSTEXPR ReturnT make_custom_exception(Ts&&... message)
+template<std::derived_from<except> ReturnT, typename... Arguments>
+WINCONSTEXPR ReturnT make_custom_exception(const std::string& message, Arguments&&... args)
 {
-	std::stringstream ss;
-	(ss << ... << std::forward<Ts>(message));
-	return{ ss.str() };
+	return ReturnT{ message, std::forward<Arguments>(args)... };
 }
 
 /**
- * @brief			Create an exception with a message.
- * @param message	Any number of types with a std::ostream& operator<<
+ * @brief			Create an exception with a given message.
+ * @tparam ...Ts	Any number of types with an overloaded operator<< for std::ostream.
+ * @param message	Any number of printable variables/objects.
  * @returns			except
  */
 template<var::Streamable<std::stringstream>... Ts>
@@ -45,10 +79,11 @@ WINCONSTEXPR except make_exception(Ts&&... message)
 }
 
 /**
- * @brief			Create an exception with a given message. 
- *\n				This function only accepts wide-char-based types, and truncates all 
- *\n				of the characters to 1 byte as exception messages cannot contain w
- * @param message	Any number of types with a std::wostream& operator<<
+ * @brief			Create an exception with a given message.
+ *\n				This function only accepts types using wchar_t instead of char, however all characters are 
+ *\n				converted to 1 byte chars before returning, as exception messages cannot contain wide chars.
+ * @tparam ...Ts	Any number of types with an overloaded operator<< for std::wostream.
+ * @param message	Any number of printable variables/objects.
  * @returns			except
  */
 template<std::derived_from<std::exception> ReturnT, var::Streamable<std::wstringstream>... Ts>
@@ -65,4 +100,3 @@ WINCONSTEXPR except make_exeption(Ts&&... message)
 		str += static_cast<char>(wch);
 	return { str.c_str() };
 }
-
